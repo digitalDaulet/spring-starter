08166a8f9bfcf24eabf5dc984df080059e0b7e23
digitalDaulet <digital.daulet@gmail.com>
2025-03-21 03:50:25 +0500

    check companyRepo bean

--- src/main/java/com/daulet/spring/ApplicationRunner.java
+++ src/main/java/com/daulet/spring/ApplicationRunner.java
@@ -1,6 +1,7 @@
 package com.daulet.spring;
 
 import com.daulet.spring.database.pool.ConnectionPool;
+import com.daulet.spring.database.repository.CompanyRepository;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class ApplicationRunner {
@@ -8,5 +9,8 @@ public class ApplicationRunner {
         var context = new ClassPathXmlApplicationContext("application.xml");
         var connectionPool = context.getBean("pool1", ConnectionPool.class);
         System.out.println(connectionPool);
+
+        var companyRepository = context.getBean("companyRepository", CompanyRepository.class);
+        System.out.println(companyRepository);
     }
 }

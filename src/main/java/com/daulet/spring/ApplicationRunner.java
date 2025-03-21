4d22ac1fb2daf6f3d099b1079b07cf8df4b4205f
digitalDaulet <digital.daulet@gmail.com>
2025-03-21 05:52:08 +0500

    passed 13th video explanation: custom bean factory post processor (bfpp)

--- src/main/java/com/daulet/spring/ApplicationRunner.java
+++ src/main/java/com/daulet/spring/ApplicationRunner.java
@@ -7,7 +7,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class ApplicationRunner {
     public static void main(String[] args) {
         // passed 13th video explanation: custom bean factory post processor (bfpp)
-        // why we need it?
+        // why do we need it?
         try (var context = new ClassPathXmlApplicationContext("application.xml")) {
             var connectionPool = context.getBean("pool1", ConnectionPool.class);
             System.out.println(connectionPool);

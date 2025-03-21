4d35e6f89967ab743bfc0a342a1b6b01cf1cf2ea
digitalDaulet <digital.daulet@gmail.com>
2025-03-21 15:19:22 +0500

    passed 15th video explanation: bean post processor

--- src/main/java/com/daulet/spring/ApplicationRunner.java
+++ src/main/java/com/daulet/spring/ApplicationRunner.java
@@ -6,7 +6,8 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class ApplicationRunner {
     public static void main(String[] args) {
-        // passed 14th video explanation: annotation based configuration
+        // passed 15th video explanation: bean post processor
+        // OMG
         try (var context = new ClassPathXmlApplicationContext("application.xml")) {
             var connectionPool = context.getBean("pool1", ConnectionPool.class);
             System.out.println(connectionPool);

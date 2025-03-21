1a9b672c658274ad5efab1e52244bab3142caaeb
digitalDaulet <digital.daulet@gmail.com>
2025-03-21 15:04:17 +0500

    passed 14th video explanation: annotation based configuration

--- src/main/java/com/daulet/spring/ApplicationRunner.java
+++ src/main/java/com/daulet/spring/ApplicationRunner.java
@@ -6,7 +6,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class ApplicationRunner {
     public static void main(String[] args) {
-        // 14th video explanation: annotation based configuration
+        // passed 14th video explanation: annotation based configuration
         try (var context = new ClassPathXmlApplicationContext("application.xml")) {
             var connectionPool = context.getBean("pool1", ConnectionPool.class);
             System.out.println(connectionPool);

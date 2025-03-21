da581bb5535927ce29744d4bb1c85fcb28ff69b6
digitalDaulet <digital.daulet@gmail.com>
2025-03-21 15:29:46 +0500

    passed 16th video explanation: custom bean post processor(bpp)

--- src/main/java/com/daulet/spring/ApplicationRunner.java
+++ src/main/java/com/daulet/spring/ApplicationRunner.java
@@ -6,8 +6,8 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class ApplicationRunner {
     public static void main(String[] args) {
-        // passed 15th video explanation: bean post processor
-        // OMG
+        // passed 16th video explanation: custom bean post processor(bpp)
+        // omg
         try (var context = new ClassPathXmlApplicationContext("application.xml")) {
             var connectionPool = context.getBean("pool1", ConnectionPool.class);
             System.out.println(connectionPool);
